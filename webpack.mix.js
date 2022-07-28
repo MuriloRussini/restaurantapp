8dfe1ebff6ae77ea55b046ce950e802370c9b082
MuriloRussini <murilorussini@usp.br>
2022-07-28 20:37:08 -0300

    transferring

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,4 @@ const mix = require('laravel-mix');
 mix .js('resources/js/app.js', 'public/js')
     .browserSync('localhost')
     .disableNotifications()
-    .sass('resources/css/styles.scss', 'public/css')
+    .sass('resources/sass/app.scss', 'public/css')
